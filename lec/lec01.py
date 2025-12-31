74edd1533e3c1bb51563aaf4be157f99d8e13a5b
Yloph <y1776980092@outlook.com>
2025-12-31 21:21:03 +0800

    Delete lec directory

--- lec/lec01.py
+++
@@ -1,8 +0,0 @@
-## All expressions can use function calls notation
-
-## Anatomy of a call expression
-# operator and operand
-
-## Evaluation procedure for call expression
-# 1.Evaluate the operator and then the operand subexpression
-# 2.Apply the function that is the value of the operator subexpression to the arguments that are the values of the operand subexpressions
